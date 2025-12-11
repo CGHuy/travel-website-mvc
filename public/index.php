46b537d35315195d4e318837ad2fda8bdcb788c3
NgocKhanh145 <nguyenngockhanh141105@gmail.com>
2025-12-11 17:23:40 +0700

    db

--- public/index.php
+++ public/index.php
@@ -87,11 +87,29 @@ function get_breadcrumbs()
         </div>
     </div>
 
-
+    <?php
+    require_once __DIR__ . '/../config/database.php';
+    $db = new Database();
+    $conn = $db->getConnection();
+    $sql = "SELECT name, cover_image FROM tours WHERE id = 5";
+    $res = $conn->query($sql);
+    if ($res && $row = $res->fetch_assoc()) {
+        $imgData = base64_encode($row['cover_image']);
+        $finfo = new finfo(FILEINFO_MIME_TYPE);
+        $mime = $finfo->buffer($row['cover_image']);
+        echo "<pre>";
+        echo "Mime-type: $mime\n";
+        echo "Blob length: " . strlen($row['cover_image']) . "\n";
+        echo "Base64 sample: " . substr($imgData, 0, 100) . "\n";
+        echo "</pre>";
+        echo '<img src="data:' . $mime . ';base64,' . $imgData . '" width="300" alt="' . htmlspecialchars($row['name']) . '">';
+    } else {
+        echo "Không tìm thấy ảnh.";
+    }
+    $db->close();
+    ?>
 </div>
 <?php
-
 include __DIR__ . '/../src/views/partials/footer.php';
 ?>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
