e5cb39f8b72be31d3a61605bd0fae691152c8dde
Hoang_Anh <kbhoanganh05@gmail.com>
2025-12-12 15:16:22 +0700

    updare menu bar

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -1,5 +1,73 @@
-<footer class="bg-light text-center text-lg-start mt-5">
-    <div class="text-center p-3">
-        © 2025 Web Du Lịch | Thiết kế bởi CGHuy
+<footer class="mt-5 border-top pt-5 pb-4 px-3 bg-white">
+    <div class="container">
+        <div class="row gy-4">
+            <div class="col-12 col-md-6 col-lg-3">
+                <div class="d-flex align-items-center gap-2 mb-3">
+                    <div style="width: 24px; height: 24px; color: #1976d2;">
+                        <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
+                            <g clip-path="url(#clip0_6_330_footer)">
+                                <path clip-rule="evenodd"
+                                    d="M24 0.757355L47.2426 24L24 47.2426L0.757355 24L24 0.757355ZM21 35.7574V12.2426L9.24264 24L21 35.7574Z"
+                                    fill="currentColor" fill-rule="evenodd"></path>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_6_330_footer">
+                                    <rect fill="white" height="48" width="48"></rect>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <h2 class="h5 fw-bold mb-0">VietTour</h2>
+                </div>
+                <p class="text-muted small mb-0">Khám phá vẻ đẹp bất tận của Việt Nam cùng chúng tôi.</p>
+            </div>
+            <div class="col-6 col-md-3">
+                <h6 class="fw-bold mb-3">Về VietTour</h6>
+                <ul class="list-unstyled mb-0">
+                    <li class="mb-2"><a class="text-muted text-decoration-none" href="#">Về chúng tôi</a></li>
+                    <li class="mb-2"><a class="text-muted text-decoration-none" href="#">Điều khoản</a></li>
+                    <li><a class="text-muted text-decoration-none" href="#">Chính sách</a></li>
+                </ul>
+            </div>
+            <div class="col-6 col-md-3">
+                <h6 class="fw-bold mb-3">Hỗ Trợ</h6>
+                <ul class="list-unstyled mb-0">
+                    <li class="mb-2"><a class="text-muted text-decoration-none" href="#">FAQ</a></li>
+                    <li class="mb-2"><a class="text-muted text-decoration-none" href="#">Liên hệ</a></li>
+                    <li><a class="text-muted text-decoration-none" href="#">Trung tâm trợ giúp</a></li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 col-lg-3">
+                <h6 class="fw-bold mb-3">Kết Nối</h6>
+                <div class="d-flex gap-3">
+                    <a class="text-muted fs-5" href="#" aria-label="Facebook">
+                        <svg aria-hidden="true" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path clip-rule="evenodd"
+                                d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
+                                fill-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                    <a class="text-muted fs-5" href="#" aria-label="Twitter">
+                        <svg aria-hidden="true" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path
+                                d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.71v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84">
+                            </path>
+                        </svg>
+                    </a>
+                    <a class="text-muted fs-5" href="#" aria-label="Instagram">
+                        <svg aria-hidden="true" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                            <path clip-rule="evenodd"
+                                d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.024.06 1.378.06 3.808s-.012 2.784-.06 3.808c-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.024.048-1.378.06-3.808.06s-2.784-.012-3.808-.06c-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.048-1.024-.06-1.378-.06-3.808s.012-2.784.06-3.808c.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 016.345 2.525c.636-.247 1.363-.416 2.427-.465C9.793 2.013 10.147 2 12.315 2zm-1.002 11.666a4.666 4.666 0 109.332 0 4.666 4.666 0 00-9.332 0z"
+                                fill-rule="evenodd"></path>
+                            <path d="M16.95 6.516a1.44 1.44 0 100 2.88 1.44 1.44 0 000-2.88z"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <hr class="my-4">
+        <div class="text-center text-muted small">
+            © 2024 VietTour. All rights reserved.
+        </div>
     </div>
 </footer>
--- src/views/partials/menu.php
+++ src/views/partials/menu.php
@@ -1,19 +1,34 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light mb-3">
-    <div class="container">
-        <a class="navbar-brand" href="/index.php">Web Du Lịch</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= route('user.index'); ?>">Danh sách người dùng</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="<?= route('user.show', ['id' => 1]); ?>">Chi tiết người dùng (demo)</a>
-                </li>
-            </ul>
+<header
+    class="d-flex align-items-center justify-content-between border-bottom px-4 py-3 sticky-top bg-white bg-opacity-80 backdrop-blur-sm z-50"
+    style="top:0;">
+    <div class="d-flex align-items-center gap-3">
+        <div class="me-2" style="width: 32px; height: 32px; color: #1976d2;">
+            <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" width="32" height="32">
+                <g clip-path="url(#clip0_6_330)">
+                    <path clip-rule="evenodd"
+                        d="M24 0.757355L47.2426 24L24 47.2426L0.757355 24L24 0.757355ZM21 35.7574V12.2426L9.24264 24L21 35.7574Z"
+                        fill="currentColor" fill-rule="evenodd"></path>
+                </g>
+                <defs>
+                    <clipPath id="clip0_6_330">
+                        <rect fill="white" height="48" width="48"></rect>
+                    </clipPath>
+                </defs>
+            </svg>
+        </div>
+        <h2 class="fs-4 fw-bold mb-0">VietTour</h2>
+    </div>
+    <nav class="d-none d-lg-flex flex-grow-1 justify-content-center align-items-center gap-4">
+        <a class="text-decoration-none text-body fw-medium px-2 py-1" href="#">Tours</a>
+        <a class="text-decoration-none text-body fw-medium px-2 py-1" href="#">Điểm đến</a>
+        <a class="text-decoration-none text-body fw-medium px-2 py-1" href="#">Về chúng tôi</a>
+        <a class="text-decoration-none text-body fw-medium px-2 py-1" href="#">Liên hệ</a>
+    </nav>
+    <div class="d-flex align-items-center gap-2">
+        <button class="d-none d-sm-flex btn btn-primary rounded-pill px-4 fw-bold">Đăng nhập</button>
+        <button class="d-none d-sm-flex btn btn-light rounded-pill px-4 fw-bold border">Đăng ký</button>
+        <div class="rounded-circle overflow-hidden"
+            style="width:40px; height:40px; background-size:cover; background-position:center; background-image:url('https://lh3.googleusercontent.com/aida-public/AB6AXuDdAJYOADieIt84Dfw2UfqcTV7w7ziE9Z_M8fnRnU5DUYpmTYKJByFLrgMNTyV-dmXqV8QiAEHkoxyFXXfrXW-ZkDSUPD94DsS9y9lVaNcg3Dwx7oSqOzXab-3F0ioQ8hsIUyE9dgLWi72PBmYwtF7S-jF1F6SU0XnlFUbMYkjmm2jfb75xwcTqy_VXIY4yeyMmKckuJltzR3DSaiDibhWUI-ZfzweEbmccIzvpUL0jCJvJcPPPOaY1x3MexpdWC1yB50j4wCwwHafQ');">
         </div>
     </div>
-</nav>
+</header>
